c38a4fc2e8b1c3bac0845f5db03c8273e4ff0468
Crustacean <elkyem@gmail.com>
2019-05-10 13:22:20 +0300

    commit new

--- printOut/print.java
+++ printOut/print.java
@@ -13,9 +13,8 @@ public class print {
 		System.out.println("Displaying your results...");
 		System.out.print( "\n" );
 		//int n=2;
-		for (int i=1; i<=10; i++) {
-		System.out.println(n +"x" +i +" = "+n*i);		
-		//System.out.print( "\n" );
+		for (int i=2; i<10; i++) {
+		System.out.println(n +"x" +i +" = "+n*i);
 		}		
 		System.out.print( "\n" );
 		System.out.println("Finished.");	
